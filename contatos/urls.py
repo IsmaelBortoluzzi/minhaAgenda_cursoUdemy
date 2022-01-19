b33f42050491ced65c341f23a3c421f2d5dfb3ec
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-01-19 08:46:03 -0300

    Levantando erro 404 se uma página não for encontrada invés do erro 500

--- contatos/urls.py
+++ contatos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contatos, name='contatos')
+    path('', views.contatos, name='contatos'),
+    path('<int:contato_id>', views.ver_contato, name='ver_contato'),
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contato
 
 
@@ -6,4 +6,12 @@ def contatos(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/contatos.html', {
         'contatos': contatos,
+    })
+
+
+def ver_contato(request, contato_id):
+    # contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
     })
