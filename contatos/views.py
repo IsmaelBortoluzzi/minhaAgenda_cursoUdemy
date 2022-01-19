d138e4e47b4e5d237d44aa47c14e38e733b9030a
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-01-19 10:57:19 -0300

    Adicionado filtro que mostra os contatos por ordem decrescente de ID

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Contato
+from django.http import  Http404
 
 
 def contatos(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
     paginator = Paginator(contatos, 5)
 
     page = request.GET.get('p')
@@ -18,6 +19,10 @@ def contatos(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
